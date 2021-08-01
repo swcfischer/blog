e2b589c7cbba8ff125b2d99cfe8302d2d2627f38
Steven FIscher <stevenfischer@Stevens-MacBook-Pro.local>
2021-08-01 10:18:47 -0500

    new post and 404 page change

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import Layout from '../components/layout'
 
-// Get this component working
-
 const NotFoundPage = () => (
   <Layout>
     <h1>We sorry! That page does not exist!</h1>
