5405ee6a6e9408b2cbe3d81bd00f5dc4048041fc
Steven Fischer <swcfischer@gmail.com>
2021-10-03 11:27:43 -0500

    about to switch to mdx

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'gatsby'
-import ThemeToggler from './ThemeToggler'
+// import ThemeToggler from './ThemeToggler'
 
 import {
   Parent,
